fc20f13fd8953fad3238bde3a59cf0c1d32773d5
Hendy Irawan <hendy@hendyirawan.com>
2017-11-07 13:38:26 +0700

    Fix: TypeError: '<' not supported between instances of 'str' and 'float'
    Make it usable in Django 1.11.7

--- src/qartez/__init__.py
+++ src/qartez/__init__.py
@@ -5,5 +5,5 @@ __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
 
 from django.utils import version
 
-if version.get_version() < 1.9:
+if isinstance(version.get_version(), float) and version.get_version() < 1.9:
     from .sitemaps import *
