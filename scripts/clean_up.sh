c27b2966d7eaeac920521aa5cbe886651a2b702d
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-11-27 21:06:43 +0100

    update ignore patterns

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
 find . -name "*.pyc" -exec rm -rf {} \;
+find . -name "__pycache__" -exec rm -rf {} \;
+find . -name "*.orig" -exec rm -rf {} \;
+find . -name "*.py,cover" -exec rm -rf {} \;
 rm -rf build/
 rm -rf dist/
+rm -rf .cache/
+rm -rf htmlcov/
 rm -rf src/*.egg-info
